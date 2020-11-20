77221787dc7c7ad023b4af907087334574a9c249
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2020-11-20 15:29:22 +0700

    created : cdn tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Hello</header>
-      <div className=""></div>
+      <div className="bg-gray-700">DPK</div>
     </div>
   );
 }
